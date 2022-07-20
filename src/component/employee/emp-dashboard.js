dd882c08d095d7dd613987ba79bbd01ca4cea045
Ramudgar <ramudgary52@gmail.com>
2022-07-21 00:21:09 +0545

    employ dashboard is prepared

--- src/component/employee/emp-dashboard.js
+++ src/component/employee/emp-dashboard.js
@@ -1,8 +1,56 @@
-function EmpDashboard(){
-    
-        return (
-            <>
-              <div id="topnavbar">
+function EmpDashboard() {
+
+    return (
+        <>
+            <div class="col-md-9">
+                <div class="card card2 p-3">
+                    <div class="hello d-flex justify-content-end align-items-center mt-3">
+                        <span>Hi,Frank Jack</span>
+                    </div>
+                    <div class="d-flex flex-row gap-3">
+                        <div class="card cardchild mt-3 p-2 px-3 py-3">
+                            <div class="d-flex p-2 mt-2 justify-content-between rounded">
+                                <div class="d-flex flex-column"><span class="type">Minimal</span><span class="number">132</span></div>
+                                <div class="d-flex flex-column"><img src="https://i.imgur.com/Slxu74c.png" class="logo1" height="40" width="40" /><span class="percentage">45%</span></div>
+                            </div>
+                        </div>
+                        <div class="card cardchild mt-3 p-2 px-3 py-3">
+                            <div class="d-flex p-2 mt-2 justify-content-between rounded">
+                                <div class="d-flex flex-column">
+                                    <span class="type">Mild</span>
+                                    <span class="number">120</span>
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <img src="https://i.imgur.com/7SEdq7z.png" class="logo2" height="40" width="40" />
+                                    <span class="percentage2">25%</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card cardchild mt-3 p-2 px-3 py-3">
+                            <div class="d-flex p-2 mt-2 justify-content-between rounded">
+                                <div class="d-flex flex-column">
+                                    <span class="type">Moderate</span>
+                                    <span class="number">25</span></div>
+                                <div class="d-flex flex-column">
+                                    <img src="https://i.imgur.com/xvUzRjK.png" class="logo3" height="40" width="40" />
+                                    <span class="percentage3">17%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card cardchild mt-3 p-2 px-3 py-3">
+                            <div class="d-flex p-2 mt-2 justify-content-between rounded">
+                                <div class="d-flex flex-column">
+                                    <span class="type">Severe</span>
+                                    <span class="number">5</span></div>
+                                <div class="d-flex flex-column">
+                                    <img src=" https://i.imgur.com/iLU5F9A.png" class="logo4" height="40" width="40" />
+                                    <span class="percentage4">13%</span>
+                                </div> </div> </div> </div>
+
+                
+            
+            <div id="topnavbar">
             <div className="d-flex align-items-center mb-3 px-md-3 px-2">
                  <span className="text-uppercase fs13 fw-bolder pe-3">search<span className="ps-1">by</span>
                  </span>
@@ -106,9 +154,10 @@ function EmpDashboard(){
                 <div className="d-flex justify-content-end bg-bdark fs13"> <span className="pe-1">Show</span> <input className="input-10" type="number" value="25"></input> <span className="ps-2"><span className="pe-2">/</span>Page</span> </div>
             </div>
         </div>
-            
-            
-            </>
-        )
-    }
+        </div>
+    </div>
+
+        </>
+    )
+}
 export default EmpDashboard;
